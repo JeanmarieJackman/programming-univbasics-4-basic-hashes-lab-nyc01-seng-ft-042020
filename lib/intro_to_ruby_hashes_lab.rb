a4653577bc8bc4d610e40b27e65103e5c8151538
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-12 20:07:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,11 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  grocery_list = {
-    bananas: 7
-    apples: 7
-    grapes: "1 bag"
-  }
+ 
 end
 
 def pioneer
